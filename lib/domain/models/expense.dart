0de6b7b3608c77afbce6b4aca3dc2043e769358b
phamvannam <pvnam198@gmail.com>
2024-04-14 11:26:29 +0700

    init

---
+++ lib/domain/models/expense.dart
@@ -0,0 +1,11 @@
+class Expense {
+  String id;
+  String detail;
+  String amount;
+  String date;
+  String categoryId;
+  String personId;
+
+  Expense(this.id, this.detail, this.amount, this.date, this.categoryId,
+      this.personId);
+}
---
+++ lib/domain/models/income.dart
@@ -0,0 +1,11 @@
+class Income {
+  String id;
+  String detail;
+  String amount;
+  String date;
+  String categoryId;
+  String personId;
+
+  Income(this.id, this.detail, this.amount, this.date, this.categoryId,
+      this.personId);
+}
